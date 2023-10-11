9ccfcacd874519e8022797b96967454a233ebb5d
Zach Gentner <zach.gentner@gmail.com>
2023-10-10 20:07:33 -0400

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+const App = () => {
+    return true;
+}
+
+
+let comp = (
+<ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+</ul>
+)
+
+const myComponent = function() {
+    return(<div>String of Text</div>);
+}
+
+ReactDOM.render(comp, document.getElementById('root'));
+ReactDOM.render(
+<div>
+    {comp}
+    {myComponent()}
+    <h1>Testing</h1>
+</div>, document.getElementById('root'));
